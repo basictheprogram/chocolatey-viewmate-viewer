837909c6a2f49915210fce0edacfedc14c61c8b0
Bob Tanner <tanner@real-time.com>
2021-07-16 14:42:12 -0500

    The installer used to be behind a username/password ftp site. Now it's open for https download. Submitting the package to the chocolatey community feed.

---
+++ tools/data.ps1
@@ -0,0 +1,5 @@
+@{
+    url          = 'https://downloads.pentalogix.com/ViewMate_Setup.zip'
+    checksum     = 'd6a9bdf7bb7c6315acb58198654de1456d25891a1d2f0eab41e6bf9de0e92d94'
+    checksumType = 'sha256'
+}
